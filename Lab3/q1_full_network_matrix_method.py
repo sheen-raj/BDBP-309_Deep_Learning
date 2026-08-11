68140ff14b673f239a0a4ba8722d6042aab3cea7
Sheenraj <sheensagacious@gmail.com>
2026-08-11 15:20:50 +0530

    Lab3 q1 completed. Hard coded 2 hidden + 1 output layer. Weights were initialised using He initialization strategy and biases were randomly initialized.

--- Lab3/q1_full_network_matrix_method.py
+++ Lab3/q1_full_network_matrix_method.py
@@ -7,7 +7,7 @@ import numpy as np
 inputs = 1, 2, 3, 4
 y = 25
 lr = 0.01
-epochs = 10
+epochs = 100
 num_layers = 3
 num_neurons_1 = 3
 num_neurons_2 = 2
@@ -15,12 +15,18 @@ num_neurons_3 = 1
 
 # Converting the inputs and weights into vectors
 X = np.array(inputs).reshape(1,4)
-W1 = np.random.randn(4, 3)
-B1 = np.random.randn(1, 3)
-W2 = np.random.randn(3, 2)
-B2 = np.random.randn(1, 2)
-W3 = np.random.randn(2, 1)
-B3 = np.random.randn(1, 1)
+Y = np.array([[y]])
+
+# He initialization for weights
+W1 = np.random.randn(4, 3) * np.sqrt(2 / 4)
+B1 = np.random.rand(1, 3)
+
+W2 = np.random.randn(3, 2) * np.sqrt(2 / 3)
+B2 = np.random.rand(1, 2)
+
+W3 = np.random.randn(2, 1) * np.sqrt(2 / 2)
+B3 = np.random.rand(1, 1)
+
 
 
 # Activation function
@@ -29,29 +35,29 @@ def ReLU(z):
 
 # Derivative of Loss function
 def loss_der(y_pred):
-    return y_pred - y
+    return y_pred - Y
 
 # Derivative of ReLU function
 def ReLU_der(z):
     return (z > 0).astype(float)
 
-def for_pass(inputs, weights, bias):
+def for_pass(X, W1, B1, W2, B2, W3, B3):
     Z1 = X @ W1 + B1
     A1 = ReLU(Z1)
     Z2 = A1 @ W2 + B2
     A2 = ReLU(Z2)
     Z3 = A2 @ W3 + B3
-    A3 = ReLU(Z3)
+    A3 = Z3
 
     y_pred = A3
     # Loss
-    loss = 0.5 * (y_pred - y)**2
+    loss = 0.5 * (y_pred - Y)**2
     return Z1, A1, Z2, A2, Z3, A3, y_pred, loss
 
-def back_pass(X, W1, B1, W2, B2, W3, B3, y_pred, Z1, A1, Z2, A2, Z3):
+def back_pass(X, W1, B1, W2, B2, W3, B3, y_pred, Z1, A1, Z2, A2):
     # Output layer
     dA3 = loss_der(y_pred)
-    dZ3 = dA3 * ReLU_der(Z3)
+    dZ3 = dA3
 
     dW3 = A2.T @ dZ3
     dB3 = dZ3
@@ -83,13 +89,22 @@ def back_pass(X, W1, B1, W2, B2, W3, B3, y_pred, Z1, A1, Z2, A2, Z3):
 
 
 for epoch in range(epochs):
-    z, loss, y_pred = for_pass(inputs, weights, bias)
-    print(y_pred.shape)
-    weights_updated, updated_bias = back_pass(inputs, weights, bias, y_pred, z)
+    # Forward pass
+    Z1, A1, Z2, A2, Z3, A3, y_pred, loss = for_pass(X,W1, B1,
+        W2, B2,W3, B3)
+
+    # Backward pass
+    W1, B1, W2, B2, W3, B3 = back_pass(X, W1, B1, W2, B2,
+        W3, B3, y_pred, Z1, A1, Z2, A2)
+
     print(f"\nEpoch {epoch + 1}")
     print(f"Prediction : {y_pred}")
     print(f"Loss       : {loss}")
-    print(f"Weights    : {weights_updated}")
-    print(f"Bias       : {updated_bias}")
-    weights = weights_updated
-    bias = updated_bias
+
+    # print(f"W1 shape   : {W1.shape}")
+    # print(f"W2 shape   : {W2.shape}")
+    # print(f"W3 shape   : {W3.shape}")
+    #
+    # print(f"B1 shape   : {B1.shape}")
+    # print(f"B2 shape   : {B2.shape}")
+    # print(f"B3 shape   : {B3.shape}")
